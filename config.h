31830da0d35c579370b1cfcc3f3c1232259a838a
Eero af Heurlin <eero.afheurlin@iki.fi>
2023-04-04 22:31:10 +0300

    modified the vial atmega32u4 example for single-button keyboard

---
+++ config.h
@@ -0,0 +1,40 @@
+/* SPDX-License-Identifier: GPL-2.0-or-later */
+
+#pragma once
+
+/* USB Device descriptor parameter */
+#define VENDOR_ID       0xFEED
+#define PRODUCT_ID      0x0000
+#define DEVICE_VER      0x0001
+#define MANUFACTURER    Rambo
+#define PRODUCT         Ejector
+
+/* key matrix size */
+/*
+#define MATRIX_ROWS 2
+#define MATRIX_COLS 2
+
+#define MATRIX_ROW_PINS { D7, E6 }
+#define MATRIX_COL_PINS { B4, B5 }
+
+#define DIODE_DIRECTION COL2ROW
+*/
+
+/* key matrix size */
+#define MATRIX_ROWS 1
+#define MATRIX_COLS 1
+
+/* direct key pin */
+#define DIRECT_PINS {{ B5 }}  // D9 on the DFRobot CM beetle
+
+
+/* Mechanical locking support. Use KC_LCAP, KC_LNUM or KC_LSCR instead in keymap */
+//#define LOCKING_SUPPORT_ENABLE
+/* Locking resynchronize hack */
+//#define LOCKING_RESYNC_ENABLE
+
+#define DEBOUNCE 5
+
+/* Use 100hz polling */
+#define USB_POLLING_INTERVAL_MS 10
+
---
+++ keymaps/vial/config.h
@@ -0,0 +1,9 @@
+/* SPDX-License-Identifier: GPL-2.0-or-later */
+
+#pragma once
+
+#define VIAL_KEYBOARD_UID {0xA4, 0x3E, 0xD1, 0x26, 0x7C, 0x05, 0x7B, 0x33}
+/*
+#define VIAL_UNLOCK_COMBO_ROWS {0, 1}
+#define VIAL_UNLOCK_COMBO_COLS {0, 1}
+*/
